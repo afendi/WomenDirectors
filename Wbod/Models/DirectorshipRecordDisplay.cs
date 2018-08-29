eb007d6eaa701792749a1973fe2460285c37ece9
afendi <fendi@ukm.edu.my>
2018-08-29 19:36:44 +0800

    add view component display for individual director's directorship record per RA's request.

---
+++ Wbod/Models/DirectorshipRecordDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.EntityFrameworkCore;
+using Wbod.Models.DB;
+
+namespace Wbod.Models
+{
+    public class DirectorshipRecordDisplay : ViewComponent
+    {
+        private readonly WbodContext _context;
+
+        public DirectorshipRecordDisplay (WbodContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke(int Id)
+        {
+            string DrId = RouteData.Values["id"] as string;
+            int DId = Int32.Parse(DrId);
+            var result = _context.Directorship.AsNoTracking().Where(p => p.Directorid == DId)
+                .Select(p => new DirectorshipRecordVM
+                {
+                    Id = p.Id,
+                    Sessionyear = p.Session.Sessionyear,
+                    Companyname = p.Company.Companyname
+                });
+
+            return View(result);
+
+        }
+    }
+}
---
+++ Wbod/Models/DirectorshipRecordVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wbod.Models
+{
+    public class DirectorshipRecordVM
+    {
+        public int Id { get; set; }
+        public string Sessionyear { get; set; }
+        public string Companyname { get; set; }
+    }
+}
